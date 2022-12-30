c6e2df0ade8322d49a5ecdd14ba3886dcf05e593
jrhe1213 <jiarong1213@gmail.com>
2022-12-29 21:38:24 -0500

    me page nav bar list v

--- patient-app/src/features/me/components/MeContainer.tsx
+++ patient-app/src/features/me/components/MeContainer.tsx
@@ -4,6 +4,10 @@ import { Box, Button, IconButton, Typography } from '@mui/material'
 import React, { useCallback, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import BannerImage1 from 'assets/images/banner/banner-1.jpg'
+import BannerImage10 from 'assets/images/banner/banner-10.jpg'
+import BannerImage9 from 'assets/images/banner/banner-9.jpg'
+
 const MAX_WIDTH = 390
 const AVATAR_ICON_SIZE = 36
 
@@ -11,7 +15,8 @@ const InfoBanner = () => (
   <Box
     component="div"
     sx={{
-      background: '#4373E6',
+      background:
+        'linear-gradient(5deg, rgba(102,182,204,1) 0%, rgba(83,148,188,1) 35%, rgba(63,115,172,1) 100%)',
       padding: '12px',
       paddingBottom: '60px',
       display: 'flex',
@@ -163,7 +168,8 @@ const NaviBanner = () => (
             <Box
               component="div"
               sx={{
-                background: '#4373E6',
+                background:
+                  'linear-gradient(5deg, rgba(102,182,204,1) 0%, rgba(83,148,188,1) 35%, rgba(63,115,172,1) 100%)',
                 borderRadius: '6px',
                 height: '48px',
                 width: '100%',
@@ -198,6 +204,160 @@ const NaviBanner = () => (
   </Box>
 )
 
+const iconNavigatorListV2: IconNavigator[] = [
+  {
+    icon: <Icon icon="uil:bill" style={{ color: 'white', fontSize: '24px' }} />,
+    title: 'Payment',
+    link: '/payment',
+  },
+  {
+    icon: <Icon icon="ic:round-history" style={{ color: 'white', fontSize: '24px' }} />,
+    title: 'History',
+    link: '/history',
+  },
+  {
+    icon: <Icon icon="gg:pill" style={{ color: 'white', fontSize: '24px' }} />,
+    title: 'Medication',
+    link: '/medication',
+  },
+  {
+    icon: <Icon icon="ant-design:insurance-filled" style={{ color: 'white', fontSize: '24px' }} />,
+    title: 'Insurance',
+    link: '/insurance',
+  },
+  {
+    icon: <Icon icon="carbon:medication" style={{ color: 'white', fontSize: '24px' }} />,
+    title: 'Refill',
+    link: '/refill',
+  },
+]
+
+const ConsultBanner = () => (
+  <Box
+    component="div"
+    sx={{
+      marginLeft: '12px',
+      marginRight: '12px',
+    }}
+  >
+    <Box
+      component="div"
+      sx={{
+        background: 'white',
+        width: '100%',
+        borderRadius: '12px',
+        padding: '12px',
+        overflow: 'hidden',
+        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+      }}
+    >
+      {/* title */}
+      <Box
+        component="div"
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: '18px',
+        }}
+      >
+        <Typography
+          component="div"
+          sx={{
+            fontSize: '12px',
+            fontFamily: 'Playfair',
+            fontWeight: 'bold',
+          }}
+        >
+          Consultation
+        </Typography>
+        {/* more btn */}
+        <Button sx={{ padding: 0, minWidth: '20px' }} onClick={() => {}}>
+          <Typography
+            component="div"
+            sx={{
+              fontSize: '9px',
+              fontFamily: 'Playfair',
+              textDecoration: 'underline',
+              color: 'black',
+            }}
+          >
+            More
+          </Typography>
+        </Button>
+      </Box>
+      {/* nav icons */}
+      <Box
+        component="div"
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+        }}
+      >
+        {iconNavigatorListV2.map((nav, index) => (
+          <Box key={index} component="div">
+            <Box
+              component="div"
+              sx={{
+                width: '32px',
+                height: '32px',
+                position: 'relative',
+                margin: '0 auto',
+              }}
+            >
+              <Box
+                component="div"
+                sx={{
+                  background:
+                    'linear-gradient(5deg, rgba(102,182,204,1) 0%, rgba(83,148,188,1) 35%, rgba(63,115,172,1) 100%)',
+                  borderRadius: '6px',
+                  height: '32px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <IconButton sx={{}}>{nav.icon}</IconButton>
+              </Box>
+            </Box>
+            <Typography
+              component="div"
+              sx={{
+                marginTop: '6px',
+                fontSize: '9px',
+                fontFamily: 'Playfair',
+                fontWeight: 'bold',
+                cursor: 'pointer',
+                textAlign: 'center',
+              }}
+            >
+              {nav.title}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+      {/* ad banner */}
+      <Box component="div" sx={{ marginTop: '12px' }}>
+        <Box
+          component="img"
+          sx={{
+            cursor: 'pointer',
+            display: 'block',
+            objectFit: 'cover',
+            width: '100%',
+            borderRadius: '12px',
+            overflow: 'hidden',
+          }}
+          alt={'ad banner image'}
+          src={BannerImage9}
+        />
+      </Box>
+    </Box>
+  </Box>
+)
+
 export const MeContainer = () => {
   console.log('123')
 
@@ -213,7 +373,11 @@ export const MeContainer = () => {
     >
       <Box
         component="div"
-        sx={{ width: '100%', maxWidth: `${MAX_WIDTH}px`, background: '#F8F8F8' }}
+        sx={{
+          width: '100%',
+          maxWidth: `${MAX_WIDTH}px`,
+          background: '#F8F8F8',
+        }}
       >
         <Box component="div" sx={{ position: 'relative', marginBottom: '54px' }}>
           {/* info banner */}
@@ -221,16 +385,58 @@ export const MeContainer = () => {
           {/* navi banner */}
           <NaviBanner />
         </Box>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
+        {/* ad banner */}
+        <Box
+          component="div"
+          sx={{
+            paddingLeft: '12px',
+            paddingRight: '12px',
+            marginTop: '72px',
+            marginBottom: '18px',
+          }}
+        >
+          <Box
+            component="img"
+            sx={{
+              cursor: 'pointer',
+              display: 'block',
+              objectFit: 'cover',
+              width: '100%',
+              borderRadius: '12px',
+              overflow: 'hidden',
+              boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+            }}
+            alt={'ad banner image'}
+            src={BannerImage1}
+          />
+        </Box>
+        {/* consult */}
+        <ConsultBanner />
+        {/* ad banner */}
+        <Box
+          component="div"
+          sx={{
+            paddingLeft: '12px',
+            paddingRight: '12px',
+            marginTop: '18px',
+            marginBottom: '72px',
+          }}
+        >
+          <Box
+            component="img"
+            sx={{
+              cursor: 'pointer',
+              display: 'block',
+              objectFit: 'cover',
+              width: '100%',
+              borderRadius: '12px',
+              overflow: 'hidden',
+              boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+            }}
+            alt={'ad banner image'}
+            src={BannerImage10}
+          />
+        </Box>
       </Box>
     </Box>
   )
